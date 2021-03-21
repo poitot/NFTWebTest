6e7c9fc5311fc9b29ba0890da83ce826b14f4d24
Connor <connortennant97@gmail.com>
2021-03-21 01:54:24 +0000

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname__ + 'index.html'));
+});
+
+app.listen(port, () -> console.log('Listening on port $(port)'));
+
