d10108cde9d65b4d0fccb1280450e5ce0f8d8309
Connor <connortennant97@gmail.com>
2021-03-21 03:05:04 +0000

    'test'

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
+
+  app.use(express.static(__dirname + '\\src'));
+  res.sendFile(path.join(__dirname + '\\index.html'));
+
 });
 
 app.listen(port, () => console.log('Listening on port $(port)'));
