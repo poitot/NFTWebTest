a0b09d8bde0e587af12f344b7e53b1f008d8305f
Connor <connortennant97@gmail.com>
2021-03-27 23:56:05 +0000

    mobile/tablet layout added in styles.css

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const port = 3000;
 
 app.get('/', function(req, res) {
 
-  app.use(express.static(__dirname + '\\src'));
-  res.sendFile(path.join(__dirname + '\\index.html'));
+  app.use(express.static(__dirname + '\\src\\assets'));
+  res.sendFile(path.join(__dirname + '\\src\\index.html'));
 
 });
 
